78a29185b355929c6a62e34437f3a410c19c7469
sangledev95 <lqsang19952@gmail.com>
2025-08-18 16:34:37 +0700

    Fix page muon tra hsba

--- src/app/(emr)/muon-tra-hsba/page.tsx
+++ src/app/(emr)/muon-tra-hsba/page.tsx
@@ -1,65 +1,37 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Box,
-  TextField,
-  Button,
-  IconButton,
-  Tabs,
-  Tab,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Checkbox,
-  Pagination,
-  Select,
-  MenuItem,
-  FormControl,
-  InputAdornment,
-  FormLabel,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-} from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import AddIcon from "@mui/icons-material/Add"; // Dùng cho nút "Làm mới"
-import DateRangeIcon from "@mui/icons-material/DateRange"; // Icon cho DatePicker
-import FirstPageIcon from "@mui/icons-material/FirstPage";
-import LastPageIcon from "@mui/icons-material/LastPage";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import {
-  HighlightOff,
-  History,
-  NoteAdd,
-  Refresh,
-  Search,
-} from "@mui/icons-material";
-import { DataGrid, GridColDef, GridRowParams } from "@mui/x-data-grid";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { getChiTietHSBA, getHosobenhan, getmuontraHSBA } from "@/actions/emr_hosobenhan";
+import { getHosobenhan, getmuontraHSBA } from "@/actions/emr_hosobenhan";
 import { IHoSoBenhAn } from "@/model/thosobenhan";
 import { ITMuonTraHSBA } from "@/model/tmuontrahsba";
-import { IUserItem } from "@/model/user";
 import { ISelectOption } from "@/model/ui";
 import { DataManager } from "@/services/DataManager";
 import { useUserStore } from "@/store/user";
+import { History, NoteAdd, Search } from "@mui/icons-material";
 import LockOpenIcon from "@mui/icons-material/LockOpen";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  Typography,
+} from "@mui/material";
+import {
+  DataGrid,
+  GridColDef,
+  GridRowParams,
+  GridRowSelectionModel,
+} from "@mui/x-data-grid";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { useEffect, useState } from "react";
 import DsMuonHsba from "./components/ds-muon-hsba";
 import LsMuonTraHsba from "./components/ls-muon-tra-hsba";
-import DsTraHsba from "./components/ds-tra-hsba";
-import { se } from "date-fns/locale";
-
 
 const columns: GridColDef[] = [
   { field: "ID", headerName: "ID", width: 60 },
@@ -114,36 +86,23 @@ const columns: GridColDef[] = [
   { field: "TenLoaiLuuTru", headerName: "Loại lưu trữ", width: 200 },
   { field: "SoNamLuuTru", headerName: "Số năm lưu trữ", width: 150 },
 ];
-export default function muontrahsbaPage() {
-
+export default function MuonTraHsbaPage() {
   const [khoaList, setKhoaList] = useState<ISelectOption[]>([]);
   const [tuNgay, setTuNgay] = useState<Date | null>(new Date());
   const [denNgay, setDenNgay] = useState<Date | null>(new Date());
   const [rows, setRows] = useState<IHoSoBenhAn[]>([]);
   const [popt, setPopt] = useState("1"); // 1: Ngày vào viện, 2: Ngày ra viện
 
-  const [currentTab, setCurrentTab] = React.useState("export");
-  const [searchTerm, setSearchTerm] = React.useState(""); // Tên tài liệu
-  const [searchStatus, setSearchStatus] = React.useState(""); // Tình trạng xét xuất
-  const [searchTuNgay, setSearchTuNgay] = React.useState("");
-  const [searchDenNgay, setSearchDenNgay] = React.useState("");
-  const [searchDateType, setSearchDateType] = React.useState("ngayVaoVien"); // Default search by "Ngày vào viện"
-
   const { data: loginedUser } = useUserStore();
   const [searchingData, setSearchingData] = useState<boolean>(false);
   // State và hàm cho Pagination
-  const [page, setPage] = React.useState(1);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
   const [isOpenDsMuonHsba, setIsOpenDsMuonHsba] = useState(false);
   const [isOpenLsMuonTraHsba, setIsOpenLsMuonTraHsba] = useState(false);
-  const [isOpenDsTraHsba, setIsOpenDsTraHsba] = useState(false);
-  const [selectedHsbaForDetail, setSelectedHsbaForDetail] = useState<IHoSoBenhAn | null>(null);
-  const [phieumuontraHSBA, setPhieumuontraHSBA] = useState<ITMuonTraHSBA | null>(null);
+  const [selectedHsbaForDetail, setSelectedHsbaForDetail] =
+    useState<IHoSoBenhAn | null>(null);
+  const [phieumuontraHSBA, setPhieumuontraHSBA] =
+    useState<ITMuonTraHSBA | null>(null);
   const [selectedKhoa, setSelectedKhoa] = useState("all"); // Trạng thái chọn khoa
-  const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
-    setCurrentTab(newValue);
-  };
 
   // Hàm tìm kiếm hồ sơ bệnh án
   const handleSearch = async () => {
@@ -180,7 +139,6 @@ export default function muontrahsbaPage() {
     }
   };
 
-
   const fetchKhoaList = async () => {
     try {
       const dataKhoaPhong = await DataManager.getDmKhoaPhong();
@@ -190,19 +148,11 @@ export default function muontrahsbaPage() {
       setKhoaList([{ value: "all", label: "Tất cả" }]);
     }
   };
-  // Hàm xử lý khi chọn rows trong DataGrid
-  const handleRowSelectionChange = async (selectionModel: any) => {
-    let selectedIds = [];
 
-    if (selectionModel && selectionModel.type === 'include' && selectionModel.ids) {
-      selectedIds = Array.from(selectionModel.ids);
-    } else if (Array.isArray(selectionModel)) {
-      selectedIds = selectionModel;
-    }
-
+  const handleRowSelected = async (selectedIds: unknown[]) => {
     if (selectedIds.length > 0) {
       const selectedId = selectedIds[0];
-      const selectedRowData = rows.find(row => row.id === selectedId);
+      const selectedRowData = rows.find((row) => row.ID === selectedId);
 
       if (selectedRowData) {
         setSelectedHsbaForDetail(selectedRowData);
@@ -233,6 +183,21 @@ export default function muontrahsbaPage() {
     }
   };
 
+  // Hàm xử lý khi chọn rows trong DataGrid
+  const handleRowSelectionChange = async (
+    selectionModel: GridRowSelectionModel
+  ) => {
+    let selectedIds: unknown[] = [];
+
+    if (selectionModel && selectionModel.ids) {
+      selectedIds = Array.from(selectionModel.ids);
+    } else if (Array.isArray(selectionModel)) {
+      selectedIds = selectionModel;
+    }
+
+    handleRowSelected(selectedIds);
+  };
+
   // Hàm xác định loại dựa trên trạng thái phiếu
   const getLoaiPhieu = () => {
     if (!phieumuontraHSBA) return "MUON"; // Nếu chưa có phiếu thì là mượn mới
@@ -247,12 +212,8 @@ export default function muontrahsbaPage() {
 
   // Hàm xử lý double click
   const handleRowDoubleClick = (params: GridRowParams) => {
-    //console.log("Row double clicked:", params.row);
+    handleRowSelected([params.row.id]);
 
-    // Gọi handleRowSelectionChange với array chứa ID của row được double click
-    handleRowSelectionChange([params.row.id]);
-
-    // Mở dialog mượn HSBA
     setIsOpenDsMuonHsba(true);
   };
 
@@ -260,32 +221,6 @@ export default function muontrahsbaPage() {
   useEffect(() => {
     fetchKhoaList();
   }, []);
-  const handleRefresh = () => {
-    console.log("Refresh clicked!");
-  };
-
-
-  // Lấy dữ liệu cho trang hiện tại
-  const paginatedData = rows.slice(
-    (page - 1) * rowsPerPage,
-    (page - 1) * rowsPerPage + rowsPerPage
-  );
-  const totalPages = Math.ceil(rows.length / rowsPerPage);
-
-  const handleChangePage = (
-    event: React.ChangeEvent<unknown>,
-    newPage: number
-  ) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(1); // Reset về trang 1 khi thay đổi số hàng mỗi trang
-  };
-
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
@@ -399,7 +334,7 @@ export default function muontrahsbaPage() {
             onClick={() => setIsOpenDsMuonHsba(true)}>
             Mượn trả HSBA
           </Button>
-          
+
           <Button
             variant="contained"
             startIcon={<History />}
@@ -407,7 +342,6 @@ export default function muontrahsbaPage() {
             onClick={() => setIsOpenLsMuonTraHsba(true)}>
             Lịch sử mượn trả
           </Button>
-          
         </Box>
 
         {/* Main Content Area (Padding around the table) */}
@@ -421,6 +355,7 @@ export default function muontrahsbaPage() {
             density="compact"
             onRowSelectionModelChange={handleRowSelectionChange}
             onRowDoubleClick={handleRowDoubleClick}
+            loading={searchingData}
             sx={{
               "& .MuiDataGrid-columnHeaders": {
                 backgroundColor: "#f5f5f5",
@@ -440,7 +375,7 @@ export default function muontrahsbaPage() {
           open={isOpenLsMuonTraHsba}
           onClose={() => setIsOpenLsMuonTraHsba(false)}
           selectedHsbaId={selectedHsbaForDetail?.ID?.toString()} // Truyền ID của HSBA được chọn
-        />         
+        />
       </Box>
     </LocalizationProvider>
   );
